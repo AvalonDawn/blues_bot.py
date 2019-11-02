028e3759a7983ac6c57bc2ed6fed8c030b0804d5
Zed Chance <zchance@sierracollege.edu>
2019-11-02 14:33:37 -0700

    Updated the update_bot script

--- update_bot.sh
+++ update_bot.sh
@@ -2,10 +2,15 @@
 echo "Updating bot..."
 cd /home/pi/blues_bot.py
 git fetch
-git pull
+git pull origin master
 
 # Update embed_help
-git submodule update --recursive
+# git submodule update --recursive
+cd cogs/embed_help/
+git stash
+git fetch
+git pull
+git stash pop
 echo "Updated."
 
 # Restart service

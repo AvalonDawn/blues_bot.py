01ab3688ba08162de8c01c399df84076f8691aaf
Zed Chance <zchance@sierracollege.edu>
2019-12-30 11:18:00 -0800

    start_bot.sh now logs

--- start_bot.sh
+++ start_bot.sh
@@ -1,4 +1,9 @@
 # Start bot
+date
 echo "Starting bot..."
+echo >> bbot.log
+echo "NEW LOG :: `whoami`" >> bbot.log
+echo `date` >> bbot.log
+python3.7 main.py >> bbot.log
+
 #sudo python3.7 /home/pi/blues_bot.py/main.py
-python3.7 main.py
